d3030b4f4e1b9e1e498bfd8b85bd16ba57b1c4fc
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-05-08 16:19:49 +0300

    ready framework

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,6 +1,4 @@
 <?php require __DIR__ . '/../inc/header.php';?>
 <h1><?php echo $data['title']; ?></h1>
-<?php foreach ($data['posts'] as $post) {?>
-  <h2><?php echo $post->title; ?></h2>
-<?php } ?>
+
 <?php require __DIR__ . '/../inc/footer.php';?>
